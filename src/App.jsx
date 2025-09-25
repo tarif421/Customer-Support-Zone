731b514d6caa80efbc1d571a531a0cdbb3743889
tarif421 <tarif305@gmail.com>
2025-09-25 14:37:15 +0600

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <div class="navbar  shadow-sm max-w-[1280px] mx-auto py-2">
+        <div class="flex-1">
+          <a class="btn btn-ghost text-xl">CS — Ticket System</a>
+        </div>
+        <div class="">
+          <ul>
+            <li className="  flex gap-3 text-xs items-center">
+              <a>Home</a>
+              <a>FAQ</a>
+              <a>Changelog</a>
+              <a>Blog</a>
+              <a>Download</a>
+              <a>Contact</a>
+              <button className="text-white bg-gradient-to-r from-[#632EE3] to-[#9F62F2] p-2 ">
+                + New Ticket
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
